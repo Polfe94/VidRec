c3d5c5b23861bae9f7fea2d6d5ae4b32bb70e4d5
Polfe94 <pfernandez@ceab.csic.es>
2022-03-09 16:53:37 +0100

    calibration.py ready to be checked

--- calibration.py
+++ calibration.py
@@ -1,44 +1,209 @@
 import os
 from math import cos, sin, pi
-# from math import sqrt
 import numpy as np
 import cv2
+from time import sleep
 import json
 from PIL import Image
 import psutil
 from copy import deepcopy
+from multiprocessing import Pool
 
-# from multiprocessing import Pool
 
-#from Ants18S import _expPth, _dtaPth, _calPth, _expLst
-from Ants18S import _nCams
-from Ants18S import _tblSzmm, _vwrImgSz
-# from Ants18S import _tblImgSz
 
-from calRun import calRun
-import vCams as fvs
-
-from vMachine import vMachine
-# from vWatcher import vWatcher
-
-# local _expPth, _calPth
-_expPth = '/home/bigtracker/tracking/runs' + os.sep
-_calPth = '/home/bigtracker/tracking/cals' + os.sep
-
-# if os.path.exists(_expPth):
-# 	_expLst = [exp for exp in sorted(os.listdir(_expPth))]
-# else:
-# 	_expLst = []
-
-# pattern-0 cell size (mm)
+''' GLOBAL VARIABLES '''
+_nCams, _nRows, _nCols = 12, 4, 3
+_camLayout = np.arange(_nCams).reshape(_nRows, _nCols)
+_tblSzmm = (2000, 2000)
+_vwrImgSz = (900, 900)
+_px2mm = 0.17857 ## NEED TO CALCULATE "DE NOVO"
+_mm2px = 1 /_px2mm
+#_mm2px = 5.68
+_tblImgSz = (int(_tblSzmm[0] *_mm2px), int(_tblSzmm[1] *_mm2px))
+_rawImgSz = (4000, 3000)
+_bffImgSz = (800, 600)
+_bufferSz = 10 # 120	
 _cellSzmm = 50
-# image border width in mm
 _bordermm = 5
 
 
-class vTuner(vMachine):
 
-	def __init__(self, exp = '20200310M', expPth = _expPth, calPth = _calPth):
+''' CONFIGURATION PATHS '''
+_expPth = '/home/bigtracker/tracking/runs' + os.sep
+_calPth = '/home/bigtracker/tracking/cals' + os.sep
+
+
+
+''' CAM WORKER FUNCTIONS '''
+def wrkr_init(key, run, imgSz, bufferSz):
+	global _Cam
+	_Cam = Cam(key, run)
+
+	_Cam.start()
+	_Cam.homography(imgSz)
+	_Cam.setBff(bufferSz, _rawImgSz)
+
+	_Cam.imgWndw = _Cam.getCrop(_vwrImgSz, _camLayout)
+	_Cam.imgSize = (_Cam.imgWndw[1] - _Cam.imgWndw[0],
+					_Cam.imgWndw[3] - _Cam.imgWndw[2])
+
+def wrkr_setRes(imgSz):
+	sleep(.003)
+	_Cam.homography(imgSz)
+	return _Cam.key
+
+def wrkr_adjust(imgSz, info):
+	sleep(.003)
+	_Cam.info = info[_Cam.key]
+	_Cam.homography(imgSz)
+	return _Cam.key
+
+def wrkr_clrFrm(f):
+	sleep(.003)
+	return (_Cam.hCrop, _Cam.readBff(f=f, color=True))
+
+def wrkr_getFrm(key):
+	ok = _Cam.wrtBff()
+	return (cv2.resize(_Cam.readBff(), _Cam.imgSize), _Cam.imgWndw)
+
+def wrkr_stop(key):
+	sleep(.003)
+	_Cam.stop()
+	return _Cam.key
+
+
+
+''' CAM CLASS '''
+class Cam():
+
+	def __init__(self, key, run):
+		self.key = key							# cam array number
+		self.expPth = run.expPth				# video path
+		self.dtaPth = run.dtaPth
+
+		self.info = run.info[self.key]
+		self.mm2px = run.info[99]
+
+		self.sn = str(self.info[0])
+		self.fila = (int)(self.info[9])
+		self.col = (int)(self.info[10])
+
+		print('+++ Cam %02i %s, initialized' % (self.key, self.sn))
+
+	def __repr__(self):
+		camInfo = '\n'
+		camInfo += '+++ Cam %s \n' % str(self.key).zfill(2)
+		camInfo += '    serialNumber ... %s \n' % self.sn
+		camInfo += '    fila ........... %02i \n' % self.fila
+		camInfo += '    col ............ %02i \n' % self.col
+
+		return camInfo
+
+	def fileName(self):
+		try:
+			for fname in os.listdir(self.expPth):
+				if self.sn in fname: break
+			return self.expPth + fname
+		except:
+			print('+++ Cam %02i %s, ' % (self.key, self.sn), end='')
+			print('NO video file found !!!')
+
+	def homography(self, imgSz=_tblImgSz):
+		pts_src = np.array(self.info[3:7])  # tl, tr, bl, br
+
+		w, h = int(self.info[7] * self.mm2px), int(self.info[8] * self.mm2px)
+		pts_dst = np.array([[0, 0], [w, 0], [0, h], [w, h]], dtype='float64')
+
+		xOff, yOff = int(self.info[1] * self.mm2px), int(self.info[2] * self.mm2px)
+		pts_dst += np.array([xOff, yOff])
+
+		# homography image-size factor
+		hFctr = np.array([imgSz[0]/_tblImgSz[0], imgSz[1]/_tblImgSz[1]])
+		pts_dst *= hFctr
+
+		try:
+			self.hMtx, status = cv2.findHomography(pts_src, pts_dst)
+			self.hSize = imgSz
+			self.hCrop = self.getCrop(self.hSize, _camLayout)
+		except:
+			print('+++ Cam %02i %s, ' % (self.key, self.sn), end='')
+			print('Error in findHomography()')
+			print(pts_src)
+			print(pts_dst)
+
+	def start(self):
+		self.frame = -1
+		try:
+			self.vid = cv2.VideoCapture(self.fileName())
+		except:
+			print('+++ Cam %02i %s, ' % (self.key, self.sn), end='')
+			print('Error starting Capture !!')
+
+	def stop(self):
+		if self.vid.isOpened(): self.vid.release()
+
+	def getCrop(self, fullSz, layout):
+		f, c = np.where(layout == self.key)[0][0], np.where(layout == self.key)[1][0]
+		a = int(round(fullSz[0] / _nCols * c, 0))
+		b = int(round(fullSz[0] / _nCols * (c + 1), 0))
+		c = int(round(fullSz[1] / _nRows * f, 0))
+		d = int(round(fullSz[1] / _nRows * (f + 1), 0))
+		return (a, b, c, d)
+
+	def setBff(self, bufferSz, bffImgSz):
+		self.bufferSz, self.bffImgSz = bufferSz, bffImgSz
+		self.iBuffer = [
+			np.zeros((self.bffImgSz[1], self.bffImgSz[0])), 3] * self.bufferSz
+		# self.iBuffer = [np.zeros((self.bffImgSz[1], self.bffImgSz[0]))] * self.bufferSz
+		self.bffPntr = -1
+
+	def readBff(self, f=0, color=False):
+		bffPntr = (self.bffPntr - f) % self.bufferSz
+		a, b, c, d = self.hCrop
+		if not color:
+			return cv2.warpPerspective(cv2.cvtColor(self.iBuffer[bffPntr], cv2.COLOR_BGR2GRAY), self.hMtx, self.hSize)[c:d, a:b]
+		else:
+			return cv2.warpPerspective(self.iBuffer[bffPntr], self.hMtx, self.hSize)[c:d, a:b, :]
+		# return cv2.warpPerspective(self.iBuffer[bffPntr], self.hMtx, self.hSize)[c:d, a:b]
+
+
+
+''' PREVIOUS CALIBRATION CLASS '''
+class calRun():
+
+	def __init__(self, exp, expPth, calPth):
+
+		if exp[-1] != os.sep: exp += os.sep
+		if not os.path.exists(expPth + exp):
+			print ('Experiment not found !!')
+			return
+
+		self.exp = exp
+		self.expPth = expPth + exp
+		self.dtaPth = ''
+		
+		self.info = {}
+		self.calFile = ''
+		for calFile in sorted(os.listdir(calPth)):
+			if calFile[:9] <= self.exp[:9]: self.calFile = calFile
+
+		with open(calPth + self.calFile) as f:
+			for key, val in json.load(f).items():
+				self.info[int(key)] = val
+
+		self.cArray = []
+		self.tags = {}
+		self.N = 0
+
+	def __repr__(self):
+		return '+++ Run %s\n' % self.expPth
+
+
+
+''' CALIBRATION CLASS '''
+class vTuner():
+
+	def __init__(self, exp='20200310M', expPth=_expPth, calPth=_calPth):
 
 		self.run = calRun(exp, expPth, calPth)
 		if not self.run: return
@@ -46,12 +211,11 @@ class vTuner(vMachine):
 		self.viewer = 'vPlayer'
 		self.w, self.h = _vwrImgSz
 
-		self.x2px, self.y2px = self.w /_tblSzmm[0], self.h/_tblSzmm[1]
+		self.x2px, self.y2px = self.w / _tblSzmm[0], self.h/_tblSzmm[1]
 
 		self.info = self.run.info
 		self.newInfo = deepcopy(self.info)
 
-		self.idLoc = {}
 		self.mm2px = 0.0
 		if 99 in self.info.keys(): self.mm2px = self.info[99]
 
@@ -62,24 +226,73 @@ class vTuner(vMachine):
 		print('+++ step 1. run self.setmm2px() to set the pixels/mm ratio if necessary;')
 		print('+++ step 2. run self.calCam(camkey) to calibrate cams one-by-one.')
 
-	def camInfo(self, cam):
+	def start(self):
+		self.pool = Pool(processes=_nCams)
+		outQ = [self.pool.apply(wrkr_init, args=(
+			k, self.run, self.imgSz, self.bufferSz)) for k in range(_nCams)]
 
+		self.frame, self.started = -1, True
+
+	def setRes(self, r):
+		self.bffImgSz = (_bffImgSz[0] * r, _bffImgSz[1] * r)
+		self.bufferSz = _bufferSz
+		self.imgSz = (self.bffImgSz[0] * _nCols, self.bffImgSz[1] * _nRows)
+		self.mrkSz = 4
+
+		if self.started:
+			outQ = self.pool.map(wrkr_setRes, [self.imgSz] * _nCams)
+			if sum(outQ) != sum(range(_nCams)):
+				print('+++ (_setRes) pool mapping error !!')
+
+	def init(self):
+		cv2.namedWindow(self.viewer, cv2.WINDOW_NORMAL)
+		cv2.resizeWindow(self.viewer, self.w, self.h)
+		cv2.moveWindow(self.viewer, 1750, 100)
+		# img. numpy.shape is (h,w)
+		self.img = np.zeros((self.h, self.w, 3), dtype='uint8')
+		self.img[:, :, :] = 160
+
+		self.frame += 1
+		self.inited = True
+
+		outQ = self.pool.map_async(wrkr_getFrm, range(_nCams)).get()
+		for img, (a, b, c, d) in outQ:
+			for q in [0, 1, 2]: self.img[c:d, a:b, q] = img
+
+		self.strtImg = np.copy(self.img)
+
+		cv2.imshow(self.viewer, self.img)
+		cv2.waitKey(1)
+
+	def stop(self):
+		if self.started:
+			outQ = self.pool.map(wrkr_stop, range(_nCams))
+			self.pool.terminate()
+
+		if self.inited:
+			cv2.destroyWindow(self.viewer)
+
+		self.frame = -1
+		self.started, self.inited = False, False
+
+	def camInfo(self, cam):
 		print('+++ Cam%02d: ' % cam)
-		print(' tl (%4d, %4d) ... tr (%4d, %4d)' % (self.newInfo[cam][3][0], self.newInfo[cam][3][1], self.newInfo[cam][4][0], self.newInfo[cam][4][1]))
-		print(' bl (%4d, %4d) ... br (%4d, %4d)' % (self.newInfo[cam][5][0], self.newInfo[cam][5][1], self.newInfo[cam][6][0], self.newInfo[cam][6][1]))
+		print(' tl (%4d, %4d) ... tr (%4d, %4d)' % (
+			self.newInfo[cam][3][0], self.newInfo[cam][3][1], self.newInfo[cam][4][0], self.newInfo[cam][4][1]))
+		print(' bl (%4d, %4d) ... br (%4d, %4d)' % (
+			self.newInfo[cam][5][0], self.newInfo[cam][5][1], self.newInfo[cam][6][0], self.newInfo[cam][6][1]))
 
 	def setmm2px(self):
-
 		'''
 		This is the first necessary step:
 		- ask for the x-coordinates of the top-left and top-right corners
 		of one of the squares in the image.
 		- knowing the size of a square (_cellSzmm = 50 mm), the pixels/mm ratio is:
-		 	(top-left -top-right) /_cellSzmm;
+			 (top-left -top-right) /_cellSzmm;
 		'''
 
 		# open Cam 0 (could be anyone, we assume all of them at the same height)
-		_Cam = fvs.Cam(0, self.run)
+		_Cam = Cam(0, self.run)
 		_Cam.vid = cv2.VideoCapture(_Cam.fileName())
 		ok, frm = _Cam.vid.read()
 		Image.fromarray(frm).show()
@@ -89,7 +302,7 @@ class vTuner(vMachine):
 
 		xRefs = input('     x1, x2 :')
 		xRefs = [int(pix) for pix in xRefs.split(',')]
-		self.mm2px = (xRefs[1] -xRefs[0]) /_cellSzmm
+		self.mm2px = (xRefs[1] - xRefs[0]) / _cellSzmm
 		self.newInfo[99] = self.mm2px
 		print(' +++ Current ratio : %6.4f px./mm ' % self.mm2px)
 
@@ -100,8 +313,20 @@ class vTuner(vMachine):
 		_Cam.vid.release()
 		_Cam.stop()
 
-	def getCamInfo(self, key, frm):
+	def focusCrop(self, fx, fy):
+		a, b = int(fx -_vwrImgSz[0] /2), int(fx +_vwrImgSz[0] /2)
+		c, d = int(fy -_vwrImgSz[1] /2), int(fy +_vwrImgSz[1] /2)
+		if a < 0:
+			a, b = 0, _vwrImgSz[0]
+		elif b > self.imgSz[0]:
+			a, b = self.imgSz[0] -_vwrImgSz[0], self.imgSz[0]
+		if c < 0:
+			c, d = 0, _vwrImgSz[1]
+		elif d > self.imgSz[1]:
+			c, d = self.imgSz[1] -_vwrImgSz[1], self.imgSz[1]
+		return (a, b, c, d)
 
+	def getCamInfo(self, key, frm):
 		cellCols = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T']
 
 		Image.fromarray(frm).show()
@@ -155,12 +380,11 @@ class vTuner(vMachine):
 		return info
 
 	def calCam(self, key):
-
 		'''
-		One-by-one calibration of the cams. New calibration paameters are saved in self.newInfo
+		One-by-one calibration of the cams. New calibration parameters are saved in self.newInfo
 		'''
 
-		_Cam = fvs.Cam(key, self.run)
+		_Cam = Cam(key, self.run)
 		self.newInfo[key].append(_Cam.sn)
 		_Cam.vid = cv2.VideoCapture(_Cam.fileName())
 		ok, frm = _Cam.vid.read()
@@ -180,7 +404,6 @@ class vTuner(vMachine):
 		_Cam.stop()
 
 	def refPattern(self, pattern = 1):
-
 		''' +++ draw reference pattern '''
 
 		x2px, y2px = self.imgSz[0] /_tblSzmm[0], self.imgSz[1] /_tblSzmm[1]
@@ -219,7 +442,6 @@ class vTuner(vMachine):
 					cv2.line(self.img, (int(x0 *x2px), int(y0 *y2px)), (int(x3 *x2px), int(y3 *y2px)), (124, 0, 0), 1)
 
 	def tune(self, pattern = 0):
-
 		if not self.started: self.start()
 		if not self.inited: self.init()
 
@@ -232,7 +454,7 @@ class vTuner(vMachine):
 			x2px, y2px = self.imgSz[0] /_tblSzmm[0], self.imgSz[1] /_tblSzmm[1]
 
 			# +++ get frame image
-			imgQ = self.pool.map_async(fvs.wrkr_clrFrm, [0] *_nCams).get()
+			imgQ = self.pool.map_async(wrkr_clrFrm, [0] *_nCams).get()
 			for (a, b, c, d), img in imgQ: self.img[c:d, a:b, :] = img
 
 			self.refPattern(pattern = pattern)
@@ -290,7 +512,6 @@ class vTuner(vMachine):
 		self.stop()
 
 	def hAdjust(self, cam, wk):
-
 		tl, tr, bl, br = self.info[cam][3:7]
 		# horizontal (x) adjustment
 		if   wk == 113: self.info[cam][3] = (tl[0] -1, tl[1])	# q, move top-left left
@@ -312,19 +533,16 @@ class vTuner(vMachine):
 		elif wk == 107: self.info[cam][6] = (br[0], br[1] +1)	# k, move btm-rght up
 
 	def calSet(self):
-
-		outQ = [self.pool.apply(fvs.wrkr_adjust, args = (self.imgSz, self.info)) for k in range(_nCams)]
+		outQ = [self.pool.apply(wrkr_adjust, args = (self.imgSz, self.info)) for k in range(_nCams)]
 		if sum(outQ) != sum(range(_nCams)):
 			print('+++ (_adjust) pool mapping error !!')
 
 	def calSave(self, cal):
-
 		if cal:
 			with open(_calPth + '%s.json' % cal , 'w') as f: json.dump(self.info, f)
 			print('+++ saved %s' % (_calPth + '%s.json' % cal))
 
 	def newCalSave(self, cal):
-
 		self.newInfo[99] = self.mm2px
 		self.info = self.newInfo
 		self.calSave(cal)
