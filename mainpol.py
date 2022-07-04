be0f91effea40b97d29ad7cd823b14b2d53aa8d9
Polfe94 <pfernandez@ceab.csic.es>
2022-07-04 13:26:16 +0200

    Adding trycatch to Joan version

--- mainpol.py
+++ mainpol.py
@@ -1,20 +1,21 @@
 import sys
 sys.path.append('/home/bigtracker/VidRec')
 import time
+import numpy as np
 
 import PySpin
 from queue import Queue
 from threading import Thread
 import cv2
-from PIL import Image
 
 import config
+# from script import SingleCam
 
 _system = PySpin.System.GetInstance()
 _cam_list = _system.GetCameras()
 
-_videoPath = config.vidPath
-_videoName = 'prova.avi'
+_videoPath = './'
+_videoName = 'prova'
 
 def _cam_release():
     for cam in _cam_list:
@@ -23,16 +24,35 @@ def _cam_release():
     _system.ReleaseInstance()
 
 
-class Cam:
+class Cam():
 
-    def __init__(self, camNum, Q, mode = 'BW'):
+    def __init__(self, camNum, Q1, Q2, Q3):
 
         self.sn = config.CamArray[camNum]
         self.camNum = camNum
-        self.Q = Q
-        self.mode = mode
+        self.Q1, self.Q2, self.Q3 = Q1, Q2, Q3
+        self.mode = config.mode
+
         self.exposure = config.exposure
         self.gain = config.gain
+
+        self.frameTimes = []
+
+    def setExp(self, exposure):
+    
+        try:
+            exposure = min(self.ptr.ExposureTime.GetMax(),(exposure*1000))
+            self.ptr.ExposureTime.SetValue(exposure)
+        except PySpin.SpinnakerException as ex:
+            print("Error: %s" % ex.what())
+
+    def setGain(self, gain):
+
+        try:
+            gain = min(self.ptr.Gain.GetMax(),gain)
+            self.ptr.Gain.SetValue(gain)
+        except PySpin.SpinnakerException as ex:
+            print("Error: %s" % ex.what())
 
     def start(self):
         
@@ -42,41 +62,38 @@ class Cam:
         except:
             print('+++ Cam %s NOT inited' % self.sn)
         
-        nodemap = self.ptr.GetNodeMap()
+        # nodemap = self.ptr.GetNodeMap()
 
-        """ Camera acquisition mode """
-        node_acquisition_mode = PySpin.CEnumerationPtr(nodemap.GetNode("AcquisitionMode"))
-        node_acquisition_mode_continuous = node_acquisition_mode.GetEntryByName("Continuous")
-        acquisition_mode_continuous = node_acquisition_mode_continuous.GetValue()
-        node_acquisition_mode.SetIntValue(acquisition_mode_continuous)
+        # """ Camera acquisition mode """
+        # node_acquisition_mode = PySpin.CEnumerationPtr(nodemap.GetNode("AcquisitionMode"))
+        # node_acquisition_mode_continuous = node_acquisition_mode.GetEntryByName("Continuous")
+        # acquisition_mode_continuous = node_acquisition_mode_continuous.GetValue()
+        # node_acquisition_mode.SetIntValue(acquisition_mode_continuous)
 
-        try:
-            if self.ptr.PixelFormat.GetAccessMode() == PySpin.RW:
-                if self.mode == 'COLOR':
-                    self.ptr.PixelFormat.SetValue(PySpin.PixelColorFilter_BayerBG) #COLOR RGB
-                else:
-                    self.ptr.PixelFormat.SetValue(PySpin.PixelFormat_Mono8)
+        # try:
+        #     if self.ptr.PixelFormat.GetAccessMode() == PySpin.RW:
+        #         if self.mode == 'COLOR':
+        #             self.ptr.PixelFormat.SetValue(PySpin.PixelColorFilter_BayerBG) #COLOR RGB
+        #         else:
+        #             self.ptr.PixelFormat.SetValue(PySpin.PixelFormat_Mono8)
 
-        except PySpin.SpinnakerException as ex:
-            print("Error: %s" % ex)
-
-
-        """ Buffer Handling """
-        nodemap_TLSdevice = self.ptr.GetTLStreamNodeMap()
-        ptrHandlingMode = PySpin.CEnumerationPtr(nodemap_TLSdevice.GetNode("StreamBufferHandlingMode"))
-        ptrHandlingModeEntry = ptrHandlingMode.GetEntryByName("NewestOnly")
-        ptrHandlingMode.SetIntValue(ptrHandlingModeEntry.GetValue())
-
-        """Exposure Time"""
-        self.ptr.ExposureAuto.SetValue(PySpin.ExposureAuto_Off)
-        self.setExp(self.exposure)
-
-        """Gain"""
-        self.ptr.GainAuto.SetValue(PySpin.GainAuto_Off)
-        self.setGain(self.gain)
+        # except PySpin.SpinnakerException as ex:
+        #     print("Error: %s" % ex)
 
 
-        ''' START ACQUIRING IMAGES '''
+        # """ Buffer Handling """
+        # nodemap_TLSdevice = self.ptr.GetTLStreamNodeMap()
+        # ptrHandlingMode = PySpin.CEnumerationPtr(nodemap_TLSdevice.GetNode("StreamBufferHandlingMode"))
+        # ptrHandlingModeEntry = ptrHandlingMode.GetEntryByName("NewestOnly")
+        # ptrHandlingMode.SetIntValue(ptrHandlingModeEntry.GetValue())
+
+        # """Exposure Time"""
+        # self.ptr.ExposureAuto.SetValue(PySpin.ExposureAuto_Off)
+        # self.setExp(self.exposure)
+
+        # """Gain"""
+        # self.ptr.GainAuto.SetValue(PySpin.GainAuto_Off)
+        # self.setGain(self.gain)
         
         self.ptr.BeginAcquisition()
         
@@ -104,43 +121,39 @@ class Cam:
         
         while self.running:
             while not self.trigger:
-                time.sleep(0.005)
-            imgPtr = self.ptr.GetNextImage()
-            self.Q.put((self.camNum, time.time(), imgPtr))
-            self.trigger = False
+                time.sleep(0.001)
+            
+            try:
+                imgPtr = self.ptr.GetNextImage()
+                self.frameTimes.append(time.time())
+                frame = imgPtr.GetNDArray()
+                imgPtr.Release()
+                self.Q1.put(frame[:1000, :])
+                self.Q2.put(frame[1000:2000:, :])
+                self.Q3.put(frame[2000:, :])
+                self.trigger = False
+            
+            except:
+                time.sleep(0.001)
 
-    def setExp(self, exposure):
-    
-        try:
-            exposure = min(self.ptr.ExposureTime.GetMax(),(exposure*1000))
-            self.ptr.ExposureTime.SetValue(exposure)
-        except PySpin.SpinnakerException as ex:
-            print("Error: %s" % ex.what())
+class Vid():
 
+    def __init__(self, vidNum, Q):
 
-    def setGain(self, gain):
+        self.vidNum = vidNum
+        self.camNum = self.vidNum //3
+        self.sn = config.CamArray[self.camNum]
+        self.vidSize = (config.vidRes[0], int(config.vidRes[1] /3))
 
-        try:
-            gain = min(self.ptr.Gain.GetMax(),gain)
-            self.ptr.Gain.SetValue(gain)
-        except PySpin.SpinnakerException as ex:
-            print("Error: %s" % ex.what())
-
-
-class VideoWriter:
-
-    def __init__(self, camNum, Q):
-
-        self.sn = config.CamArray[camNum]
-        self.camNum = camNum
         self.Q = Q
 
         self.fps = 15
-        self.videoName = '%s/%s%2d.avi' %(_videoPath, _videoName, self.camNum)
-        self.outVid = cv2.VideoWriter(self.videoName, cv2.VideoWriter_fourcc('X','V','I','D'), self.fps, config.vidRes, 0)
+        self.videoName = '%s/%s_%s%s.avi' %(_videoPath, _videoName, str(self.camNum).zfill(2), str(self.vidNum).zfill(2))
+        self.outVid = cv2.VideoWriter(self.videoName, cv2.VideoWriter_fourcc('X','V','I','D'), self.fps, self.vidSize, 0)
 
     def start(self):
                 
+        self.trigger = False
         self.running = True
 
         t = Thread(target = self.update, args = (), daemon = True)
@@ -155,111 +168,75 @@ class VideoWriter:
 
         self.outVid.release()
 
+    def getFrame(self):
+        self.trigger = True
+
     def update(self):
 
         while self.running:
+            # if not self.trigger:
             if self.Q.empty():
                 time.sleep(0.001)
             else:
                 img = self.Q.get()
                 self.outVid.write(img)
-
-        while not self.Q.empty():
-            self.outVid.write(self.Q.get())
-
-class ImageWriter:
-    
-    def __init__(self, camNum, Q):
-
-        self.sn = config.CamArray[camNum]
-        self.camNum = camNum
-        self.Q = Q
-
-        self.imagePath = _videoPath
-
-    def start(self):
-                
-        self.running = True
-
-        for i in range(5):
-            t = Thread(target = self.update, args = (), daemon = True)
-            t.start()
-
-        return self
-
-    def stop(self):
-    
-        self.running = False
-        time.sleep(0.5)
-
-    def update(self):
-
-        while self.running:
-            if self.Q.empty():
-                time.sleep(0.001)
-            else:
-                frameNum, array = self.Q.get()
-                img = Image.fromarray(array)
-                img.save(self.imagePath + 'cam_%s_frame_%s.tif' % (self.sn, frameNum))
+                # self.trigger = False
 
 
-class MainProcess:
+class MainProcess():
 
-    def __init__(self, nCams):
+    def __init__(self, nCams = 12):
 
-        self.nCams = nCams
-        self.camList = []
-        self.masterQ = [Queue(1) for k in range(self.nCams)]
-        self.imgQ = [Queue(1000) for k in range(self.nCams)]
-        self.fps = 15
+        self.nCams, self.nVids = nCams, nCams *3
+        self.camList, self.vidList = [], []
+
+        self.vidsQ = [Queue(20) for k in range(self.nVids)]
 
     def start(self):
 
-        self.camList = [Cam(k, self.masterQ[k]).start() for k in range(self.nCams)]
-        print(len(self.camList))
-        # self.frameWriterList = [VideoWriter(k, self.imgQ[k]).start() for k in range(self.nCams)]
-        self.ImageWriterList = [ImageWriter(k, self.imgQ[k]).start() for k in range(self.nCams)]
+        self.camList = [Cam(k, self.vidsQ[3 *k], self.vidsQ[3 *k +1], self.vidsQ[3 *k +2]).start() for k in range(self.nCams)]
+        self.vidList = [Vid(v, self.vidsQ[v]).start() for v  in range(self.nVids)]
 
-    def rec(self):
+    def rec(self, recTime = 900):
+
+        time.sleep(5)
 
         frameNumber = 0
-        t0 = time.time()
-
-        nextframe = t0 + 0.1
-
-        while frameNumber < 100:
-
-            while time.time() < nextframe:
-                time.sleep(0.001) 
+        startTime = time.time()
+        currTime = time.time() - startTime
+        while currTime <= recTime:
 
             [_Cam.getFrame() for _Cam in self.camList]
+            print('Frame = %s' % frameNumber)
+            while any([_Cam.trigger for _Cam in self.camList]):
+                time.sleep(0.001)
 
-            while not all([q.qsize() for q in self.masterQ]):
-                time.sleep(0.005)
-
-            minTime, maxTime = time.time(), 0
-            for Q in self.masterQ:
-                camNum, frameTime, ptr = Q.get()
-                self.imgQ[camNum].put((frameNumber, ptr.GetNDArray()))
-                ptr.Release()
-                if frameTime < minTime: minTime = frameTime
-                if frameTime > maxTime: maxTime = frameTime
-                print(frameNumber, camNum, frameTime, maxTime -minTime, self.imgQ[camNum].qsize())
+            time.sleep(0.001)
             
-            nextframe += 1/ self.fps
             frameNumber += 1
-
-        print('+++ Total time elapsed = %s seconds' % str(round(time.time() - t0, 3)))
+            currTime = time.time() - startTime
+            
+        print('Finished recording, mean FPS = %s' % frameNumber / recTime)
+        frameTimes = np.array([_Cam.frameTimes for _Cam in self.camList])
+        print(frameTimes.shape[1] /recTime)
+        print(np.mean(np.max(frameTimes, axis = 0) -np.min(frameTimes, axis = 0)))
 
     def stop(self):
 
         [_Cam.stop() for _Cam in self.camList]
-        # [_VideoWriter.stop() for _VideoWriter in self.frameWriterList]
-        [_ImageWriter.stop() for _ImageWriter in self.ImageWriterList]
+        [_Vid.stop() for _Vid in self.vidList]
         _cam_release()
 
 def go():
 
+    # _system = PySpin.System.GetInstance()
+    # _cam_list = _system.GetCameras()
+
     m = MainProcess(12)
     m.start()
     m.stop()
+
+    # for cam in _cam_list:
+    #     del cam
+    # _cam_list.Clear()
+    # _system.ReleaseInstance()
